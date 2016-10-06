5cf6847ffdbbc299e5d208a10b51511b72d8762b
Jackie Xu <jackie@aeveus.com>
2016-10-06 17:48:24 +0200

    Removed unnecessary aliasing.

--- src/AppBundle/Entity/Entry.php
+++ src/AppBundle/Entity/Entry.php
@@ -3,7 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\ORM\Mapping\UniqueConstraint as UniqueConstraint;
+use Doctrine\ORM\Mapping\UniqueConstraint;
 
 /**
  * Class Entry
